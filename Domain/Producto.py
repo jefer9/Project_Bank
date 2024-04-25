ea084e93fa3a073b6a8400f704acae1dffcbefe1
David gomez <david.gomez04@hotmail.com>
2024-04-24 19:01:10 -0500

    se agregan los metodos crear producto y mostrar producto

--- Domain/Producto.py
+++ Domain/Producto.py
@@ -1,8 +1,7 @@
 from Domain.Titular import Titular
 
-class Producto(Titular):
-    def __init__(self, id_producto, nombre_producto, producto, clave):
-        super().__init__(producto, clave)
+class Producto:
+    def __init__(self, id_producto, nombre_producto):
         self._id_producto = id_producto
         self.nombre_producto = nombre_producto
 
@@ -13,3 +12,22 @@ class Producto(Titular):
     @id_producto.setter
     def id_producto(self, id_producto):
         self._id_producto = id_producto
+
+    @property
+    def nombre_producto(self):
+        return self.nombre_producto
+
+    @nombre_producto.setter
+    def nombre_producto(self,nombre_producto):
+        self.nombre_producto = nombre_producto
+
+    productos = {}
+
+    def crear_producto(self):
+        self._id_producto = int(input("id producto: "))
+        self.nombre_producto = input("nombre producto: ")
+        self.productos[self._id_producto] = self.nombre_producto
+
+    def mostrar_producto(self):
+        for i in self.productos.items():
+            print(i)
